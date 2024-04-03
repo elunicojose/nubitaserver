2265e54ec59be0116cbaf99a5cee6d44bf2cdbac
elunicojose <elunicojose@gmail.com>
2024-04-03 01:08:50 -0300

    se cambia persistencia uso de archivo por mysql

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const app = express();
-const { loadFrutas, loadMixes } = require("./nubita_model");
-const PORT = process.env.PORT || 3001;
+require("dotenv").config();
+const mysql = require("mysql2/promise");
 var cors = require("cors");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-const frutas = loadFrutas();
-const mixes = { ...loadMixes() };
-
-app.use(express.json());
 app.use(cors());
 
-let arrFrutas = Object.values(frutas);
-
-let arrMixes = Object.values(mixes);
-
-
-const handleSaveData = (data, fileName) => {
-  console.log("Saving DATA= ", data);
-  var sData = JSON.stringify(data);
-  var thePath = path.join("./server/data/", fileName);
-  console.log('path archivo= ', thePath)
-  fs.writeFile(thePath, sData, (err) => {
-    if (err) {
-      console.log('Error while saving data: ', err);
-    } else console.log("Data saved!");
-  });
-};
-
-app.get("/frutas", (req, res) => {
-  res.send(loadFrutas());
-});
-
-app.get("/mixes", (req, res) => {
-  res.send(loadMixes());
-});
-
 app.get("/", (req, res) => {
-  res.json("Welcome");
+  res.send("<h1>Nubita!</h1>")
+})
+
+app.get("/api/frutas", (req, res) => {
+  console.log("en local app get...");
+  async function getFrutas() {
+    console.log("en remote app get...");
+    // Create the connection to database
+    const connection = await mysql.createConnection({
+      host: process.env.host || "http://localhost",
+      port: 3306,
+      database: "nubitamix",
+      user: process.env.user || "root",
+      password: process.env.password || "rootpass",
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    // A simple SELECT query
+    try {
+      const [result, fields] = await connection.query("SELECT * FROM `frutas`");
+      console.log("result: ", result); // results contains rows returned by server
+      res.set('Access-Control-Allow-Origin', '*');
+      res.json(result);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  getFrutas();
 });
 
+app.get("/api/mixes", (req, res) => {
+  // TO DO
+});
+
+
+/*
 app.post("/api/addFruta", (req, res) => {
   console.log(req.body);
   if (req.body.action == "ADD") {
@@ -62,98 +67,10 @@ app.post("/api/addFruta", (req, res) => {
   }
 
   res.status(200).json(req.body);
-});
+});*/
 
-
-app.delete("/borrarFruta", (req, res) => {
-  console.log('borrando en server...', req.body);
-  removeFruta(req.body.id, res)
-  //res.status(200).json({"remove": "ok"})
-})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-const existeFrutaEnMix = (idFruta) => {
-  const index = arrFrutas.findIndex((laFruta) => {
-    return laFruta.id === idFruta;
-  });
-
-  if (index == -1) {
-    console.log("No se pudo borrar la fruta");
-    return false;
-  }
-
-  let nombreFruta = arrFrutas[index].nombre;
-
-  for (let i = 0; i < mixes.length; i++) {
-    let mix = arrMixes[i];
-    for (let j = 0; j < mix.frutasMix.length; j++) {
-      if (
-        nombreFruta.trim().toUpperCase() ===
-        mix.frutasMix[j].nombre.trim().toUpperCase()
-      ) {
-        return true;
-      }
-    }
-  }
-  return false;
-};
-
-const removeFruta = (rec, res) => {
-  console.log("borrar= ", rec);
-  if (arrFrutas && arrFrutas.length > 0) {
-    if (existeFrutaEnMix(rec)) {
-      //showAlert("Esta fruta está agregada en un mix", "danger");
-      console.log( "Esta fruta está agregada en un mix")
-      res.status(500).json({"status": "error", "message": "Esta fruta está agregada en un mix"})
-      return;
-    }
-    
-   /* var filtro = arrFrutas.filter((elem, i) => {
-      if (rec == elem.id) {
-        arrFrutas.splice(i, 1);
-        updateFrutaID(arrFrutas);
-        //buildTable(); --> actualizar lista de frutas en frontend
-        
-        //window.electronAPI.onSaveData(frutas, "frutas.json");
-        handleSaveData(arrFrutas, "frutas.json")
-        
-        //showAlert("Fruta borrada", "info");
-        res.status(200).json({"status": "ok", "message": "Fruta borrada"})
-      }
-    });*/
-
-    const idxFrutaBorrar = arrFrutas.findIndex(elem => {
-      return elem.id === rec;
-    });
-
-    if (idxFrutaBorrar === -1) {
-      console.log("No se pudo borrar la fruta")
-      res.status(500).json({"status": "error", "message": "No se pudo borrar la fruta"})
-      return
-    }
-
-    arrFrutas.splice(idxFrutaBorrar, 1);
-    updateFrutaID(arrFrutas); 
-    handleSaveData(arrFrutas, "frutas.json")
-    console.log("Fruta borrada")
-    res.status(200).json({"status": "ok", "message": "Fruta borrada"})
-
-  }
-  console.log(arrFrutas);
-};
-
-const updateFrutaID = (frutas) => {
-  //SI SE ACTUALIZA EL ID FRUTA, ACTUALIZAR TAMBIEN EN LAS FRUTAS DE LOS MIXES
-  
-  //NO HACE FALTA EL COMENTARIO ANTERIOR, 
-  //PORQUE EL UPDATE SE HACE CUANDO SE BORRA UNA FRUTA 
-  //Y NO SE PUEDE BORRAR UNA FRUTA QUE ESTÉ INCLUIDA EN UN MIX
-  if (frutas) {
-    frutas.forEach((elem, index) => {
-      elem.id = index + 1;
-    });
-  }
-};
--- nubita_model.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-let loadFrutas = () => JSON.parse(fs.readFileSync('./data/frutas.json'));
-let loadMixes = () => JSON.parse(fs.readFileSync('./data/mixes.json'));
-
-module.exports = {loadFrutas, loadMixes}
